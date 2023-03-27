93b5f05d6b7a6e3fb613162219f7ea348c2ec329
Princekotadiya007 <princuuu77@gmail.com>
2023-03-27 08:38:09 +0530

    [prince] create data base

---
+++ AddressBookSql.sql
@@ -0,0 +1,8 @@
+--what is sql - sql is structure query language
+--SQL is standared language forstoring and retriveing data from database.
+--Database - Organized data in structural part.
+
+
+--Creating Database UC1
+CREATE DATABASE AddressBook_Service
+USE AddressBook_Service
