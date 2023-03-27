f6186c62be48aed93489a0c43bb2766d896866cc
Princekotadiya007 <princuuu77@gmail.com>
2023-03-27 08:42:11 +0530

    [prince] craete table

--- AddressBookSql.sql
+++ AddressBookSql.sql
@@ -6,3 +6,20 @@
 --Creating Database UC1
 CREATE DATABASE AddressBook_Service
 USE AddressBook_Service
+
+--Creating Table UC2
+--Primary key is uniquely identify each record table. It can't contain null value.In a table we have only one key.
+CREATE TABLE AddressBook_DB(
+PersonID INT PRIMARY KEY IDENTITY(1,1),
+FirstName VARCHAR(30) NOT NULL,
+LastName VARCHAR(30) NOT NULL,
+Address VARCHAR(150) NOT NULL,
+City VARCHAR(30) NOT NULL,
+State VARCHAR(30) NOT NULL,
+Zip INT NOT NULL,
+PhoneNumber BIGINT NOT NULL,
+Email VARCHAR(50) NOT NULL
+);
+
+--Select Query
+SELECT * FROM AddressBook_DB
